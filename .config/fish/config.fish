4d1b375036a63fe284b793ce332e55055967c479
mknurs <mikanovic@kompot.si>
2021-02-11 00:34:37 +0100

    ob theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,13 @@
 # startx at login
 if status is-login
 	if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-		exec startx "$HOME/.config/X11/xinitrc"
-	else if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
+	# start Openbox
 		exec startx "$HOME/.config/X11/ob-xinitrc"
+	else if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
+	# start Xmonad
+		exec startx "$HOME/.config/X11/xmonad-xinitrc"
 	end
 end
 
+# ALIAS
 alias config='/usr/bin/git --git-dir=/home/mkn/.cfg/ --work-tree=/home/mkn/'
