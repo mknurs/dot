c4c7c6303ab83d6c2fe05ec9768ed357bbc6edfd
mknurs <mikanovic@kompot.si>
2021-02-12 17:14:42 +0100

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 # startx at login
 if status is-login
 	if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-	# start Openbox
+	# start Openbox on tty1
 		exec startx "$HOME/.config/X11/ob-xinitrc"
 	else if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
-	# start Xmonad
+	# start Xmonad on tty2 (deprecated)
 		exec startx "$HOME/.config/X11/xmonad-xinitrc"
 	end
 end
