756c87ce4a85e24eb5d8fca18f2472cb78302ecf
mknurs <mikanovic@kompot.si>
2021-02-15 11:42:20 +0100

    rofi

--- .config/rofi/scripts/config.sh
+++ .config/rofi/scripts/config.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-files=$(/usr/bin/git --git-dir=/home/mkn/.cfg/ --work-tree=/home/mkn/ ls-tree -r --full-name --name-only master)
 chosen=$(/usr/bin/git --git-dir=/home/mkn/.cfg/ --work-tree=/home/mkn/ ls-tree -r --full-name --name-only master | rofi -theme config -dmenu -i)
 
-if [[ $chosen = "" ]]; then
+if [[ $chosen == "" ]]; then
 	exit
 else
-	nvim $chosen
+	xterm -e nvim $chosen
 fi
